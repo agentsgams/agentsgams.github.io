16697f4105f953636748040bb7c3b4d3921c1736
agentN86 <128328445+agentN86@users.noreply.github.com>
2024-10-03 19:38:29 -0500

    added cookie clicker

--- gamHTML.js
+++ gamHTML.js
@@ -38,7 +38,7 @@ if (page == '1') {
     createItem("3dmaze", "3D Maze", "./gams/3dmaze.html", "./thumbs/3dmaze.svg", "pink", "is a simple maze gam where you have to find the exit! Use WASD to move and left and right arrows to look around. Try finding the green flag to win.")
     createItem("alienshooter", "Alien Shooter", "./gams/alienshooter.html", "./thumbs/alienshooter.png", "pink", "is a simple shooter gam where the user has to shoot the alien. The more you shoot- the faster it is! Use mouse to shoot.")
     createItem("bitcoin-clicker", "Bitcoin Clicker", "https://julianyaman.github.io/bitcoin-clicker/", "./thumbs/bitcoinclicker.png", "white", "is a 'Cookie Clicker' similar type gam- but instead it's bitcoin!")
-    createItem("cookie-clicker", "Cookie Clicker", "https://steamcookieclicker.github.io/", "./thumbs/cookieclicker.jpeg", "white", "is exactly what it sounds like. You know what it is- why not play it?")
+    createItem("cookie-clicker", "Cookie Clicker", "./gams/cookieclicker", "./thumbs/cookieclicker.jpeg", "white", "is exactly what it sounds like. You know what it is- why not play it?")
     createItem("papaspizzeria", "Papa's Pizzeria", "./gams/flash/papapizzera.html", "./thumbs/papapizzeria.jpeg", "white", "is the original classic of the Papa gams. Own a pizza business and get the best scores!")
     createItem("papasburgeria", "Papa's Burgeria", "./gams/flash/papaburger.html", "./thumbs/papaburger.jpeg", "white", "is one of the sequels to Papa's Pizzeria. Why not give both a try?")
     createItem("ageofwar2", "Age of War 2", "./gams/flash/ageofwar2.html", "./thumbs/ageofwar2.jpeg", "white", "is a war simulation gam where you try to win a war aganist a enemy AI!")
