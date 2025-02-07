8abd34bc71fe4a8f84abe7cc56d515ae5d7f6ea4
agentN86 <128328445+agentN86@users.noreply.github.com>
2025-02-06 20:54:59 -0600

    funny devtools thing

--- scripts/inject.js
+++ scripts/inject.js
@@ -16,3 +16,10 @@ if (window.localStorage.hasOwnProperty("agentsgams-theme")) {
     document.body.setAttribute("data-theme", local_theme);
     console.log("%cTheme is set to: %c" + local_theme, "font-weight: bold;", "font-weight: normal;");
 };
+
+var array = ["Ah, hello Gordon Freeman!", "Hello there!", "It's great to see you again!", "hi,", "whats up reddit, its kendrick lamar", "It is nice to see you!", "I see someone is using DevTools..!", "Chrome or Firefox?", "letter from agentn86..", "[insert funny joke that you can only see if your chromebook allows the devtools setting or using a personal computer]"]
+console.warn(
+    `%c${array[Math.floor(Math.random() * array.length)]}%c You can mess around here as you wish, but if you would like to check out the source code go to https://github.com/agentsgams/agentsgams . I'll see ya there :)`,
+    "font-weight: bold;",
+    "font-weight: normal;"
+)
