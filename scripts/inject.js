7db6d5afd0e1c7cb9c4c975c25d539a45c1f9ce2
agentN86 <128328445+agentN86@users.noreply.github.com>
2025-02-06 18:13:06 -0600

    analytics and inject fixed

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,1 +1,12 @@
-const local_title = localStorage.getItem("agentsgams-title");const local_icon = localStorage.getItem("agentsgams-icon");if (window.localStorage.hasOwnProperty("agentsgams-title")) {document.title = local_title;console.log("Title is now: " + local_title);}if (window.localStorage.hasOwnProperty("agentsgams-icon")) {document.querySelector("link[rel=icon]").href = local_icon;console.log("Favicon is now: " + local_icon);}
+const local_title = localStorage.getItem("agentsgams-title");
+const local_icon = localStorage.getItem("agentsgams-icon");
+
+if (window.localStorage.hasOwnProperty("agentsgams-title")) {
+    document.title = local_title;
+    console.log("%cTitle is now: %c" + local_title, "font-weight: bold;", "font-weight: normal;");
+};
+
+if (window.localStorage.hasOwnProperty("agentsgams-icon")) {
+    document.querySelector("link[rel=icon]").href = local_icon;
+    console.log("%cFavicon is now: %c" + local_icon, "font-weight: bold;", "font-weight: normal;");
+};
