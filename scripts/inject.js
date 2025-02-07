2be63407b51286f39a9de3f58d8de421a4ed6362
agentN86 <128328445+agentN86@users.noreply.github.com>
2025-02-06 18:39:32 -0600

    WAKE UP

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,5 +1,6 @@
 const local_title = localStorage.getItem("agentsgams-title");
 const local_icon = localStorage.getItem("agentsgams-icon");
+const local_theme = localStorage.getItem("agentsgams-theme");
 
 if (window.localStorage.hasOwnProperty("agentsgams-title")) {
     document.title = local_title;
@@ -9,4 +10,9 @@ if (window.localStorage.hasOwnProperty("agentsgams-title")) {
 if (window.localStorage.hasOwnProperty("agentsgams-icon")) {
     document.querySelector("link[rel=icon]").href = local_icon;
     console.log("%cFavicon is now: %c" + local_icon, "font-weight: bold;", "font-weight: normal;");
+};
+
+if (window.localStorage.hasOwnProperty("agentsgams-theme")) {
+    document.body.setAttribute("data-theme", local_theme);
+    console.log("%cTheme is set to: %c" + local_theme, "font-weight: bold;", "font-weight: normal;");
 };
