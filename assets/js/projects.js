1b8fa1f0da373e89e240adceaa702fecc8dbaccb
agentN86 <128328445+agentN86@users.noreply.github.com>
2025-02-11 20:32:19 -0600

    lets see how this will work

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,23 +2,52 @@ import { main } from './module.js';
 var baseurl = main('BaseURL');
 var isxml = main('IsXML');
 
-function createItem(game, formatted, link, image, color, description, xmllink) {
-    var template = `
-    <br>
-    <div style="display: flex; align-items: top;">
-        <a name="${game}"></a>
-        <a onclick="openWindow('${link}', '${formatted}', ${isxml}, '${xmllink}', '${baseurl}')" style="cursor:pointer;">&nbsp;&nbsp;<img src="${image}" alt="${game}" width="100" style="border: 3px solid ${color};"></a><span class="title">&nbsp;&nbsp;<u><a style="color: white; cursor: pointer;" onclick="openWindow('${link}', '${formatted}', ${isxml}, '${xmllink}', '${baseurl}')">${formatted}</a></u></span>
-        <p1>&nbsp;${description}</p1>
-    </div>
-    `;
-
-    document.getElementById('projects').innerHTML += template;
-};
-
-function openWindow(e,t,n,o,c){console.log("Opening window with:", e, t, n, o, c);if("Eaglercraft"!=t){var i=window.open();i.document.body.style.margin="0",i.document.body.style.height="100vh",i.document.title="Redirecting...";var d=i.document.createElement("iframe");if(d.style.border="none",d.style.width="100%",d.style.height="100%",d.style.margin="0",1==n){var l=c+o;d.src=l}else d.src=e;i.document.body.appendChild(d)}else location.replace(e)};
-
 var page = document.getElementById('page').innerText;
 var TOTALGAMES = 120;
+function openWindow(e, t, n, o, c) {
+    console.log("Opening window with:", e, t, n, o, c);
+    if ("Eaglercraft" != t) {
+        var i = window.open();
+        (i.document.body.style.margin = "0"), (i.document.body.style.height = "100vh"), (i.document.title = "Redirecting...");
+        var d = i.document.createElement("iframe");
+        if (((d.style.border = "none"), (d.style.width = "100%"), (d.style.height = "100%"), (d.style.margin = "0"), 1 == n)) {
+            var l = c + o;
+            d.src = l;
+        } else d.src = e;
+        i.document.body.appendChild(d);
+    } else location.replace(e);
+}
+
+function openWindow(link, xml, formal) {
+    if (formal == "Eaglercraft") {location.replace(link)};
+    if (isxml == true) {
+        var url = baseurl + xml;
+        var win = window.open();
+        win.document.body.style.margin = "0";
+        win.document.body.style.height = "100vh";
+        win.document.title = "Redirecting..";
+        var iframe = win.document.createElement("iframe");
+        iframe.src = url
+        iframe.style.border = "none";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.margin = "0";
+        win.document.body.appendChild(iframe);
+    } else {
+        var url = link;
+        var win = window.open();
+        win.document.body.style.margin = "0";
+        win.document.body.style.height = "100vh";
+        win.document.title = "Redirecting..";
+        var iframe = win.document.createElement("iframe");
+        iframe.src = url
+        iframe.style.border = "none";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.margin = "0";
+        win.document.body.appendChild(iframe);
+    };
+};
 
 switch (page) {
     case "1":
@@ -43,15 +72,15 @@ fetch('./assets/json/projects.json')
         if (!projects) { alert('Nothing found for this page.'); return; }
 
         projects.forEach((project) => {
-            const { game, formal, description, image, color, xml } = project;
+            const { game, formal, description, image, color, xml, link } = project;
             const projectDiv=document.createElement("div");projectDiv.style.display="flex",projectDiv.style.alignItems="top";const gameLink=document.createElement("a");gameLink.name=game,gameLink.style.cursor="pointer";const projectImage=document.createElement("img");projectImage.src=image,projectImage.alt=game,projectImage.width=100,projectImage.style.border=`3px solid ${color}`,projectImage.style.cursor="pointer";const titleSpan=document.createElement("span");titleSpan.className="title",titleSpan.innerHTML=`&nbsp;&nbsp;<u><a style="color: white; cursor: pointer;">${formal}</a></u>`;const descriptionParagraph=document.createElement("p1");descriptionParagraph.innerHTML=`&nbsp;${description}`,projectDiv.appendChild(gameLink),gameLink.appendChild(projectImage),projectDiv.appendChild(titleSpan),projectDiv.appendChild(descriptionParagraph);
             
             projectImage.onclick = (e) => {
-                
+                openWindow(link, xml, formal);
             };
 
             titleSpan.querySelector('a').onclick = (e) => {
-                
+                openWindow(link, xml, formal);
             };
 
             document.getElementById('projects').appendChild(projectDiv);
