5acf3e9f1f3fa2d306bb10f0ce6125f45b937860
agentN86 <agentn86@duck.com>
2025-02-28 11:21:09 -0600

    changed module

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,38 +1,45 @@
 export function main(input) {
-    var isxml = false
-    var url = ""
-    var imgurl = ""
+    var isxml = false;
+    var url = "";
+    var imgurl = "";
     switch (window.location.host) {
         case "agentsgams.github.io":
-            url = "https://agentsgams.github.io/projects/"
-            isxml = false
-            imgurl = "https://agentsgams.github.io/agentsgams/assets/img/thumbnails/"
+            url = "https://agentsgams.github.io/projects/";
+            isxml = false;
+            imgurl = "https://agentsgams.github.io/agentsgams/assets/img/thumbnails/";
             break
         case "altagentgam.onrender.com":
-            url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url="
-            isxml = true
-            break
+            url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url=";
+            isxml = true;
+            imgurl = "https://cdn.jsdelivr.net/gh/agentsgams/agentsgams@master/assets/img/thumbnails/";
+            break;
         case "lowtaperfade.onrender.com":
-            url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url="
-            isxml = true
-            break
+            url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url=";
+            isxml = true;
+            imgurl = "https://cdn.jsdelivr.net/gh/agentsgams/agentsgams@master/assets/img/thumbnails/";
+            break;
         case "agentsgams.netlify.app":
-            url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url="
-            isxml = true
-            break
+            url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url=";
+            isxml = true;
+            imgurl = "https://cdn.jsdelivr.net/gh/agentsgams/agentsgams@master/assets/img/thumbnails/";
+            break;
         case "rainbow-liger-1ac4c7.netlify.app":
-            url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url="
-            isxml = true
-            break
+            url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url=";
+            isxml = true;
+            imgurl = "https://cdn.jsdelivr.net/gh/agentsgams/agentsgams@master/assets/img/thumbnails/";
+            break;
         default:
-            url = "https://example.com?"
-            isxml = false
-            break
-    }
+            url = "https://example.com?";
+            isxml = false;
+            imgurl = "https://example.com?assets/img/thumbnails/";
+            break;
+    };
     switch (input) {
         case "BaseURL":
-            return url
+            return url;
         case "IsXML":
-            return isxml
-    }
+            return isxml;
+        case "ImgURL":
+            return imgurl;
+    };
 }
