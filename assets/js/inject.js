7be88296940e6665e1f98e2ffbacc85568ffeee3
agentN86 <128328445+agentN86@users.noreply.github.com>
2025-03-01 17:20:13 -0600

    most cdn stuff

--- assets/js/inject.js
+++ assets/js/inject.js
@@ -1,7 +1,8 @@
 const local_title = localStorage.getItem("agentsgams-title");
 const local_icon = localStorage.getItem("agentsgams-icon");
 const local_theme = localStorage.getItem("agentsgams-theme");
-const allowAds = "true" === "true" // make it toggable soon (wip)
+const local_allowAds = "true" === "true" // make it toggable soon (wip)
+const local_cdn = localStorage.getItem("agentsgams-cdn");
 
 // Cloaker Whatnot
 if (window.localStorage.hasOwnProperty("agentsgams-title")) { document.title = local_title; console.log("%cTitle is now: %c" + local_title, "font-weight: bold;", "font-weight: normal;"); };
@@ -13,7 +14,7 @@ var array = ["Ah, hello Gordon Freeman!", "Hello there!", "It's great to see you
 console.warn( `%c${array[Math.floor(Math.random() * array.length)]}%c You can mess around here as you wish, but if you would like to check out the source code go to https://github.com/agentsgams/agentsgams.github.io . I'll see ya there :)`, "font-weight: bold;", "font-weight: normal;")
 
 // Analytics and Google Stuff
-if (allowAds) {
+if (local_allowAds) {
     console.log("Ads enabled, thank you :)")
 
     // <!-- Google tag (gtag.js) --><script async src="https://www.googletagmanager.com/gtag/js?id=G-WV5EJV9W8G"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-WV5EJV9W8G');</script>
@@ -29,4 +30,38 @@ if (allowAds) {
     document.head.append(tagmanager, tagmanagerInfo);
     console.log("Sucessfully injected Google Tag Manager");
 
+    const gglads = document.createElement("script");
+    gglads.setAttribute("async", "");
+    gglads.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4260993446105201");
+    gglads.setAttribute("crossorigin", "anonymous");
+    document.head.append(gglads);
+    console.log("Sucessfully injected Google AdSense");
+
+};
+
+// CDN Stuff
+// most of this code is from 3kh0.. credits to them!
+async function isBlocked(url) {
+    try {
+        var README = await fetch(url + 'README.md');
+        var content = await README.text();
+        if (content.startsWith('# Projects')) { return false; } else { return true; };
+    } catch { return true; };
+};
+  
+async function getCDN(cdns) {
+    for (let cdn of cdns) {
+        var blocked = await isBlocked(cdn);
+        if (!blocked) { return cdn; }
+    }
+    return cdns[0];
+};
+
+if (!window.localStorage.hasOwnProperty("agentsgams-cdn")) {
+    fetch("./assets/json/cdns.json")
+        .then((res) => res.json())
+        .then(async (cdns) => {
+            localStorage.setItem("agentsgams-cdn", await getCDN(cdns));
+            location.reload();
+        });
 };
