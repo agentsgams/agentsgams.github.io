54dc6f842b6576f1752487d694cee241f3fdaf56
agentN86 <128328445+agentN86@users.noreply.github.com>
2025-03-01 12:53:57 -0600

    changed module

--- assets/js/module.js
+++ assets/js/module.js
@@ -4,34 +4,40 @@ export function main(input) {
     var imgurl = "";
     switch (window.location.host) {
         case "agentsgams.github.io":
-            url = "https://agentsgams.github.io/projects/";
+            url = "/projects/";
             isxml = false;
-            imgurl = "https://agentsgams.github.io/assets/img/thumbnails/";
+            imgurl = "./assets/img/thumbnails/";
             break
         case "altagentgam.onrender.com":
             url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url=https://cdn.jsdelivr.net/gh/agentsgams/projectsxml@master/";
             isxml = true;
-            imgurl = "https://cdn.jsdelivr.net/gh/agentsgams/agentsgams.github.io@master/assets/img/thumbnails/";
+            imgurl = "./assets/img/thumbnails/";
             break;
         case "lowtaperfade.onrender.com":
             url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url=https://cdn.jsdelivr.net/gh/agentsgams/projectsxml@master/";
             isxml = true;
-            imgurl = "https://cdn.jsdelivr.net/gh/agentsgams/agentsgams.github.io@master/assets/img/thumbnails/";
+            imgurl = "./assets/img/thumbnails/";
             break;
         case "agentsgams.netlify.app":
             url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url=https://cdn.jsdelivr.net/gh/agentsgams/projectsxml@master/";
             isxml = true;
-            imgurl = "https://cdn.jsdelivr.net/gh/agentsgams/agentsgams.github.io@master/assets/img/thumbnails/";
+            imgurl = "./assets/img/thumbnails/";
             break;
         case "rainbow-liger-1ac4c7.netlify.app":
             url = "https://agentsgams-opensocial.googleusercontent.com/gadgets/ifr?container=ig&url=https://cdn.jsdelivr.net/gh/agentsgams/projectsxml@master/";
             isxml = true;
-            imgurl = "https://cdn.jsdelivr.net/gh/agentsgams/agentsgams.github.io@master/assets/img/thumbnails/";
+            imgurl = "./assets/img/thumbnails/";
             break;
-        default:
+        case "127.0.0.1:5500":
+            // basically localhost
             url = "https://agentsgams.github.io/projects/";
             isxml = false;
-            imgurl = "https://cdn.jsdelivr.net/gh/agentsgams/agentsgams.github.io@master/assets/img/thumbnails/";
+            imgurl = "https://agentsgams.github.io/assets/img/thumbnails/";
+            break;
+        default:
+            url = "https://example.com?PROJECT=";
+            isxml = false;
+            imgurl = "https://example.com?IMAGE=";
             break;
     };
     switch (input) {
